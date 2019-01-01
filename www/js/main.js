b30376a03e89be1551d653778fa816c500bb3069
Filavision <info@filavision.de>
2019-01-01 16:19:15 +0100

    add sync function

--- www/js/main.js
+++ www/js/main.js
@@ -1,6 +1,7 @@
 ﻿var akttime = new Date();
 var content;
 var storagetype = 0;
+var geodata = [];
 
 //initApp------------------------------------------------------------------------------------------------
 function initApp(){
@@ -28,11 +29,25 @@ geodata.push(newItem);
 geodata = objectarray_unique(geodata);
 if(storagetype==0){NativeStorage.setItem("geodata", geodata)}else{localStorage.setItem("geodata", JSON.stringify(geodata))}
 $("#savegpsbtn").css({opacity:1}).attr("onclick","saveGPS()");
+$("#syncbtn").show().css({opacity:1}).attr("onclick","sync()").html("Sync "+geodata.length+" Points");
 alert("Position saved!");
 }
 function geoErrorLive(error){
 $("#savegpsbtn").css({opacity:1}).attr("onclick","saveGPS()");
 alert("Position not found!");
+}
+
+//sync----------------------------------------------------------
+function sync(){
+$("#syncbtn").css({opacity:0.5}).attr("onclick","");
+$.ajax({url:'https://reisetagebuch.filavision.de/wp-content/plugins/fila-gpstravelroute/ajax.php',type:'POST',data:'a=sync&geodata='+encodeURIComponent(JSON.stringify(geodata)),crossDomain:true,dataType:'json',jsonp:'callback',timeout:30000,cache:false,success:function(data, status){
+if (data["mel"]==1){
+geodata=[];
+if(storagetype==0){NativeStorage.remove("geodata")}else{localStorage.removeItem("geodata")};
+$("#syncbtn").hide();
+alert("Sync complete!");
+}
+},error:function(){$("#syncbtn").show().css({opacity:1}).attr("onclick","sync()");alert("Sync error!")}});
 }
 
 //seitenaufbau--------------------------------------------------------------------------------------------------------------------
@@ -41,8 +56,14 @@ akttime = new Date();
 mySwiper.removeAllSlides();
 
 if (page=="start"){//start
-content="<h1>Save your GPS Position</h1><div class='fakebtn' onclick='saveGPS()' id='savegpsbtn'>Save GPS Point</div>";
+content="<h1>Save current Position</h1><div class='fakebtn' onclick='saveGPS()' id='savegpsbtn'>Save GPS Point</div>";
+content+="<br /><div class='fakebtn' onclick='sync()' id='syncbtn'></div>";
 mySwiper.appendSlide('<div id="content0" class="swiper-slide"><div class="p5 center">'+content+'</div></div>');
+if (geodata.length>0){
+$("#syncbtn").show().css({opacity:1}).attr("onclick","sync()").html("Sync "+geodata.length+" Points");
+}else{
+$("#syncbtn").hide();
+}
 }
 
 }
